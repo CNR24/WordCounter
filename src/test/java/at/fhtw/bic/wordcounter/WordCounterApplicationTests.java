4d76d5defb9def30093cc1927f922b58a97b6f3f
Caner <ic20b038technikum-wien.at>
2023-03-09 16:15:58 +0100

    Initial commit

---
+++ src/test/java/at/fhtw/bic/wordcounter/WordCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.wordcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
