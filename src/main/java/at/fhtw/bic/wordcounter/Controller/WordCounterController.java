d41091ddd0d2a44f01231cdb6b13036a35bfef57
Caner <ic20b038technikum-wien.at>
2023-03-09 16:29:18 +0100

    This commit make "Add new Packege and Controller Class"

---
+++ src/main/java/at/fhtw/bic/wordcounter/Controller/WordCounterController.java
@@ -0,0 +1,8 @@
+package at.fhtw.bic.wordcounter.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordCounterController {
+}
