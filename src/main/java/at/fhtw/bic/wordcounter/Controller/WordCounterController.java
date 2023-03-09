6d3a32aade2b41514100934d5ce80d1e44ff76e7
Caner <ic20b038technikum-wien.at>
2023-03-09 16:42:19 +0100

    This commit make "write te API programm finisch"

--- src/main/java/at/fhtw/bic/wordcounter/Controller/WordCounterController.java
+++ src/main/java/at/fhtw/bic/wordcounter/Controller/WordCounterController.java
@@ -1,8 +1,25 @@
 package at.fhtw.bic.wordcounter.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WordCounterController {
+        @GetMapping("/api/words")
+        public int countWords(@RequestParam String text) {
+            String[] words = text.split("\\s+");
+            return words.length;
+        }
+
+        @GetMapping("/api/letters")
+        public int countLetters(@RequestParam String text) {
+            return text.replaceAll("\\s+", "").length();
+        }
+
+        @GetMapping("/api/characters")
+        public int countCharacters(@RequestParam String text) {
+            return text.length();
+        }
 }
+
